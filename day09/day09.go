14702004522b79cf6d0bfe589015f40e7ad2b5a9
Rafaël L. Bekkema <rafael@rafaelbekkema.nl>
2020-01-16 15:38:56 +0100

    Solve Day 9, Puzzle 1

---
+++ day09/day09.go
@@ -0,0 +1,154 @@
+package day09
+
+import (
+	"aoc/utils"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func parseInput(input string) []int64 {
+	vals := strings.Split(input, ",")
+	res := make([]int64, len(vals))
+	for idx, strval := range vals {
+		num, err := strconv.ParseInt(strval, 10, 64)
+		if err != nil {
+			panic(err)
+		}
+		res[idx] = num
+	}
+	return res
+}
+
+func intcodeComp(init []int64, input <-chan int64, output chan<- int64) {
+	var pos, relBase int64
+
+	memory := make([]int64, len(init))
+	copy(memory, init)
+
+For:
+	for {
+		value := strconv.FormatInt(memory[pos], 10)
+		startFrom := 0
+		if len(value) > 2 {
+			startFrom = len(value) - 2
+		}
+		opcode, _ := strconv.Atoi(value[startFrom:])
+
+		getVal := func(offset int64) int64 {
+			mode := uint8('0')
+			if int64(len(value)) >= offset+2 {
+				idx := int64(len(value)) - (offset + 2)
+				mode = value[idx]
+			}
+
+			switch mode {
+			case '0': // Position mode
+				return memory[memory[pos+offset]]
+			case '1': // Immediate mode
+				return memory[pos+offset]
+			case '2': // Relative mode
+				return memory[relBase+memory[pos+offset]]
+			default:
+				panic(fmt.Sprintf("unknown mode: %q in op: %q", mode, value))
+			}
+		}
+
+		setVal := func(offset int64, newVal int64) {
+			mode := uint8('0')
+			if int64(len(value)) >= offset+2 {
+				idx := int64(len(value)) - (offset + 2)
+				mode = value[idx]
+			}
+
+			var targetPos int64
+			switch mode {
+			case '0': // Position mode
+				targetPos = memory[pos+offset]
+			case '2': // Relative mode
+				targetPos = relBase + memory[pos+offset]
+			default:
+				panic(fmt.Sprintf("unknown mode: %q in op: %q", mode, value))
+			}
+
+			if targetPos >= int64(len(memory)) {
+				// Extend memory
+				newSize := targetPos - int64(len(memory)) + 1
+				memory = append(memory, make([]int64, newSize)...)
+			}
+
+			memory[targetPos] = newVal
+		}
+
+		switch opcode {
+		case 1:
+			// Add
+			setVal(3, getVal(1)+getVal(2))
+			pos += 4
+		case 2:
+			// Mul
+			setVal(3, getVal(1)*getVal(2))
+			pos += 4
+		case 3:
+			//Input
+			setVal(1, <-input)
+			pos += 2
+		case 4:
+			//Output
+			output <- getVal(1)
+			pos += 2
+		case 5:
+			if getVal(1) != 0 {
+				pos = getVal(2)
+			} else {
+				pos += 3
+			}
+		case 6:
+			if getVal(1) == 0 {
+				pos = getVal(2)
+			} else {
+				pos += 3
+			}
+		case 7:
+			var val int64
+			if getVal(1) < getVal(2) {
+				val = 1
+			}
+			setVal(3, val)
+			pos += 4
+		case 8:
+			var val int64
+			if getVal(1) == getVal(2) {
+				val = 1
+			}
+			setVal(3, val)
+			pos += 4
+		case 9: // adjusts the relative base
+			relBase += getVal(1)
+			pos += 2
+		case 99:
+			// Done
+			break For
+		default:
+			panic(fmt.Sprintf("op code not recognized: %d", opcode))
+		}
+	}
+	close(output)
+}
+
+func puzzle1(data []int64) []int64 {
+	input := make(chan int64)
+	output := make(chan int64)
+	go intcodeComp(data, input, output)
+	input <- 1
+	result := make([]int64, 0, 1)
+	for val := range output {
+		result = append(result, val)
+	}
+	return result
+}
+
+func Puzzle1() []int64 {
+	data := parseInput(utils.ReadAll("./input"))
+	return puzzle1(data)
+}
---
+++ day09/day09_test.go
@@ -0,0 +1,36 @@
+package day09
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestIntcodeComp(t *testing.T) {
+	tt := []struct {
+		init, expect []int64
+	}{
+		{[]int64{109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99}, []int64{109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99}},
+		{[]int64{1102, 34915192, 34915192, 7, 4, 7, 99, 0}, []int64{1219070632396864}},
+		{[]int64{104, 1125899906842624, 99}, []int64{1125899906842624}},
+	}
+
+	for idx, tc := range tt {
+		input := make(chan int64)
+		output := make(chan int64)
+		go intcodeComp(tc.init, input, output)
+		result := make([]int64, 0, len(tc.expect))
+		for val := range output {
+			result = append(result, val)
+		}
+		if !reflect.DeepEqual(result, tc.expect) {
+			t.Errorf("test %d: expected %v, got %v", idx+1, tc.expect, result)
+		}
+	}
+}
+
+func ExamplePuzzle1() {
+	fmt.Println(Puzzle1())
+
+	// Output: [2775723069]
+}
