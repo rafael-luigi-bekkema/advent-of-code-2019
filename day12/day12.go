04f5733437b39e8786c5d4211e7c80acb2fef540
Rafaël L. Bekkema <rafael@rafaelbekkema.nl>
2020-01-21 11:51:42 +0100

    Solve Day 12, Puzzle 2 (with help)

--- day12/day12.go
+++ day12/day12.go
@@ -6,6 +6,26 @@ import (
 	"math"
 )
 
+func gcd(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+// find Least Common Multiple (LCM) via GCD
+func lcm(a, b int, integers ...int) int {
+	result := a * b / gcd(a, b)
+
+	for i := 0; i < len(integers); i++ {
+		result = lcm(result, integers[i])
+	}
+
+	return result
+}
+
 type Coord struct {
 	x, y, z int
 }
@@ -34,6 +54,19 @@ func (moon *Moon) adjustPos() {
 	moon.pos.z += moon.vel.z
 }
 
+type History struct {
+	pos, vel [4]int
+}
+
+func history(moons []*Moon) (History, History, History) {
+	return History{[4]int{moons[0].pos.x, moons[1].pos.x, moons[2].pos.x, moons[3].pos.x},
+			[4]int{moons[0].vel.x, moons[1].vel.x, moons[2].vel.x, moons[3].vel.x}},
+		History{[4]int{moons[0].pos.y, moons[1].pos.y, moons[2].pos.y, moons[3].pos.y},
+			[4]int{moons[0].vel.y, moons[1].vel.y, moons[2].vel.y, moons[3].vel.y}},
+		History{[4]int{moons[0].pos.z, moons[1].pos.z, moons[2].pos.z, moons[3].pos.z},
+			[4]int{moons[0].vel.z, moons[1].vel.z, moons[2].vel.z, moons[3].vel.z}}
+}
+
 func (moon *Moon) totalEnergy() int {
 	total := (math.Abs(float64(moon.pos.x)) + math.Abs(float64(moon.pos.y)) + math.Abs(float64(moon.pos.z))) *
 		(math.Abs(float64(moon.vel.x)) + math.Abs(float64(moon.vel.y)) + math.Abs(float64(moon.vel.z)))
@@ -49,6 +82,13 @@ func diff(a, b int) int {
 	return 0
 }
 
+func printMoons(count int, moons []*Moon) {
+	fmt.Printf("Loop %d\n", count)
+	for _, moon := range moons {
+		fmt.Println(moon)
+	}
+}
+
 func puzzle1(data []string, steps int) int {
 	moons := make([]*Moon, len(data))
 	for idx, item := range data {
@@ -56,6 +96,8 @@ func puzzle1(data []string, steps int) int {
 		_, _ = fmt.Sscanf(item, "<x=%d, y=%d, z=%d>", &moon.pos.x, &moon.pos.y, &moon.pos.z)
 		moons[idx] = &moon
 	}
+
+	printMoons(0, moons)
 
 	for i := 0; i < steps; i++ {
 		// Adjust velocities
@@ -78,7 +120,61 @@ func puzzle1(data []string, steps int) int {
 	return total
 }
 
+func puzzle2(data []string) int {
+	moons := make([]*Moon, len(data))
+	for idx, item := range data {
+		var moon Moon
+		_, _ = fmt.Sscanf(item, "<x=%d, y=%d, z=%d>", &moon.pos.x, &moon.pos.y, &moon.pos.z)
+		moons[idx] = &moon
+	}
+
+	oxh, oyh, ozh := history(moons)
+
+	var count int
+	var gotx, goty, gotz bool
+	var loopx, loopy, loopz int
+	for {
+		// Adjust velocities
+		for _, moon := range moons {
+			for _, submoon := range moons {
+				moon.adjustVel(submoon)
+			}
+		}
+
+		// Adjust positions
+		for _, moon := range moons {
+			moon.adjustPos()
+		}
+
+		count++
+		xh, yh, zh := history(moons)
+		if xh == oxh && !gotx {
+			loopx = count
+			gotx = true
+		}
+		if yh == oyh && !goty {
+			loopy = count
+			goty = true
+		}
+		if zh == ozh && !gotz {
+			loopz = count
+			gotz = true
+		}
+
+		if gotx && goty && gotz {
+			break
+		}
+	}
+
+	return lcm(loopx, loopy, loopz)
+}
+
 func Puzzle1() int {
 	data := utils.ReadLines("./input")
 	return puzzle1(data, 1000)
+}
+
+func Puzzle2() int {
+	data := utils.ReadLines("./input")
+	return puzzle2(data)
 }
--- day12/day12_test.go
+++ day12/day12_test.go
@@ -33,3 +33,32 @@ func ExamplePuzzle1() {
 
 	// Output: 10845
 }
+
+func TestPuzzle2(t *testing.T) {
+	tt := []struct {
+		data   []string
+		expect int
+	}{
+		{[]string{"<x=-1, y=0, z=2>",
+			"<x=2, y=-10, z=-7>",
+			"<x=4, y=-8, z=8>",
+			"<x=3, y=5, z=-1>"}, 2772},
+		{[]string{"<x=-8, y=-10, z=0>",
+			"<x=5, y=5, z=10>",
+			"<x=2, y=-7, z=3>",
+			"<x=9, y=-8, z=-3>"}, 4686774924},
+	}
+
+	for idx, tc := range tt {
+		result := puzzle2(tc.data)
+		if tc.expect != result {
+			t.Errorf("test %d: expected %d, got %d", idx+1, tc.expect, result)
+		}
+	}
+}
+
+func ExamplePuzzle2() {
+	fmt.Println(Puzzle2())
+
+	// Output: 551272644867044
+}
