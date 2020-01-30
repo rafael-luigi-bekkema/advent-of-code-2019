c5f8855db943e24ebaeda0d71654f0fd12d6a151
Rafaël L. Bekkema <rafael@rafaelbekkema.nl>
2020-01-30 14:21:56 +0100

    Solve Day 16

--- day16/day16.go
+++ day16/day16.go
@@ -2,6 +2,7 @@ package day16
 
 import (
 	"aoc/utils"
+	"strconv"
 )
 
 func parseInput(input string) []int {
@@ -12,40 +13,34 @@ func parseInput(input string) []int {
 	return res
 }
 
-func sequence(pos int) []int {
+func algo(arr []int) []int {
 	base := []int{0, 1, 0, -1}
-	seq := make([]int, pos*4)
-	for idx, val := range base {
-		for i := 0; i < pos; i++ {
-			seq[idx*pos+i] = val
-		}
-	}
-	return seq
-}
-
-func puzzle1(input string) string {
-	arr := parseInput(input)
 
 	for i := 0; i < 100; i++ {
 		newArr := make([]int, len(arr))
 		for idx := range arr {
-			seq := sequence(idx + 1)
 			var newVal int
 			for idx2, val := range arr {
-				seqVal := seq[(idx2+1)%len(seq)]
+				seqVal := base[((idx2+1)/(idx+1))%4]
 				newVal += val * seqVal
 			}
 			// Keep only the ones
 			if newVal < 0 {
 				newVal *= -1
 			}
-			if newVal >= 10 || newVal <= -10 {
+			if newVal >= 10 {
 				newVal = newVal % (newVal / 10 * 10)
 			}
 			newArr[idx] = newVal
 		}
 		arr = newArr
 	}
+	return arr
+}
+
+func puzzle1(input string) string {
+	arr := parseInput(input)
+	arr = algo(arr)
 
 	strArr := make([]int32, 8)
 	for i := 0; i < 8; i++ {
@@ -56,4 +51,63 @@ func puzzle1(input string) string {
 
 func Puzzle1() string {
 	return puzzle1(utils.ReadAll("./input"))
+}
+
+func mulArray(inputArr []int, mul int) []int {
+	arr := make([]int, mul*len(inputArr))
+	for i := 0; i < mul; i++ {
+		copy(arr[i*len(inputArr):], inputArr)
+	}
+	return arr
+}
+
+func ones(newVal int) int {
+	if newVal < 0 {
+		newVal *= -1
+	}
+	if newVal >= 10 {
+		newVal = newVal % (newVal / 10 * 10)
+	}
+	return newVal
+}
+
+func puzzle2(input string) string {
+	arr := mulArray(parseInput(input), 10000)
+	baseIdx, _ := strconv.Atoi(input[:7])
+
+	newArr := arr[baseIdx:]
+
+	/**
+	Two realizations:
+	1. Indeces before start index can be ignored
+	2. Because start index is always over half, the second half of the patter 0 and -1 don't come into play.
+	   The current index is always 0, and the rest are 1
+	*/
+
+	for i := 0; i < 100; i++ {
+		str := make([]int, len(newArr))
+		var total int
+		for e := range newArr {
+			if e == 0 {
+				total = 0
+				for _, f := range newArr {
+					total += f
+				}
+			} else {
+				total -= newArr[e-1]
+			}
+			str[e] = ones(total)
+		}
+		newArr = str
+	}
+
+	var strArr [8]int32
+	for i := 0; i < 8; i++ {
+		strArr[i] = int32(newArr[i] + 48)
+	}
+	return string(strArr[:])
+}
+
+func Puzzle2() string {
+	return puzzle2(utils.ReadAll("./input"))
 }
--- day16/day16_test.go
+++ day16/day16_test.go
@@ -27,3 +27,26 @@ func ExamplePuzzle1() {
 
 	// Output: 88323090
 }
+
+func TestPuzzle2(t *testing.T) {
+	tt := []struct {
+		input, expect string
+	}{
+		{"03036732577212944063491565474664", "84462026"},
+		{"02935109699940807407585447034323", "78725270"},
+		{"03081770884921959731165446850517", "53553731"},
+	}
+
+	for idx, tc := range tt {
+		result := puzzle2(tc.input)
+		if result != tc.expect {
+			t.Errorf("test %d: expected %q, got %q", idx+1, tc.expect, result)
+		}
+	}
+}
+
+func ExamplePuzzle2() {
+	fmt.Println(Puzzle2())
+
+	// Output: 50077964
+}
